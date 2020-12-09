082934b7bc737c975ac80bff01ecb056fecbeb3c
Jespejo <jorgeespejo.dev@gmail.com>
2020-12-09 16:27:25 -0300

    homework Memorize memo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { Memorize } from './components/06-memos/Memorize';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
@@ -15,6 +16,6 @@ import { CallbackHook } from './components/06-memos/CallbackHook';
 
 
 ReactDOM.render(
-    <CallbackHook />,
+    <Padre />,
   document.getElementById('root')
 );
