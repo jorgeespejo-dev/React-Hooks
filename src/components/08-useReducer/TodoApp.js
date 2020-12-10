c5f5e64850b179ea235407e9d51cacfcadb4cd70
Jespejo <jorgeespejo.dev@gmail.com>
2020-12-10 10:08:35 -0300

    create list and style ToDos

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -13,18 +13,51 @@ export const TodoApp = () => {
 
     const [ todos ] = useReducer(todoReducer, initialState);
 
-    console.log(todos)
+    console.log(todos);
 
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({ todos.length }) </h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>mundo</li>
-                <li>Hola</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {
+                            todos.map( (todo, i) => (
+                                <li
+                                    key={todo.id}
+                                    className="list-group-item"
+                                >
+                                    <p className="text-center">{i+1}. {todo.desc}</p>
+                                    <button className="btn btn-danger">
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="col">
+                    <h4>Agregar ToDo</h4>
+                    <hr />
+
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Aprender.."
+                            autoComplete="off"
+                         />
+
+                         <button className="btn btn-outline-primary mt-1 btn-block">
+                            Agregar
+                         </button>
+
+                    </form>
+                </div>
+            </div>
 
         </div>
     )
