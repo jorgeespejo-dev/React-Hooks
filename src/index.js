c53b48081f4f6a3b49755da758ff09511e7b22c0
Jespejo <jorgeespejo.dev@gmail.com>
2020-12-11 13:00:02 -0300

    Router React, Link and NavLink

--- src/index.js
+++ src/index.js
@@ -14,9 +14,10 @@ import ReactDOM from 'react-dom';
 // // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // // import { HookApp } from './HookApp';
 // import './components/08-useReducer/intro-reducer';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 
 ReactDOM.render(
-    <TodoApp />,
+    <MainApp />,
   document.getElementById('root')
 );
