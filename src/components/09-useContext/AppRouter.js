2a370d9526d8cef6337fd6dcf77762e7b85542dd
Jespejo <jorgeespejo.dev@gmail.com>
2020-12-11 17:57:31 -0300

    useContext data child

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -16,16 +16,17 @@ export const AppRouter = () => {
         <Router>
             <>
                 <NavBar />
-                
-                <Switch>
-                    <Route exact path="/" component={HomeScreen} />
+                <div className="container">
+                    <Switch>
+                        <Route exact path="/" component={HomeScreen} />
 
-                    <Route exact path="/about" component={AboutScreen} />
-                    <Route exact path="/login" component={LoginScreen} />
+                        <Route exact path="/about" component={AboutScreen} />
+                        <Route exact path="/login" component={LoginScreen} />
 
-                    <Redirect to="/" />
+                        <Redirect to="/" />
 
-                </Switch>
+                    </Switch>
+                </div>
             </>
         </Router>
     )
