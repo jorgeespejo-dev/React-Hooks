510fe22d85bc3685b2c7fa33de9899096c635e6e
Jespejo <jorgeespejo.dev@gmail.com>
2020-12-11 14:09:31 -0300

    CreateContext and useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+
+    const user ={
+        id: 1234,
+        name: 'jespejo',
+        email: 'jespejo@jdevoto.cl'
+    }
+
     return (
-        <AppRouter />
+        <UserContext.Provider value={user}>
+
+            <AppRouter />
+
+        </UserContext.Provider>
     )
 }
